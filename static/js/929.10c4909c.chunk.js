22d1a590cb1bedb689c50e62f9e8512d9ad3ddce
Glipper 000 <husdadymena@gmail.com>
2022-03-07 21:05:12 -0500

    Updates

--- static/js/929.10c4909c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktt=self.webpackChunktt||[]).push([[929],{1929:function(e,t,a){a.r(t);a(2791);var s=a(4658),l=a(7082),n=a(184),i=l.Z.Title,c=a(5080);t.default=function(){return(0,n.jsxs)(s.kC,{wrap:!0,align:"center",justify:"center",children:[(0,n.jsx)("img",{src:c,width:40,height:40,alt:"app-logo",loading:"lazy",className:"app-logo mr-5"}),(0,n.jsx)(i,{className:"app-name mb-0",children:"Movie Awards Ballot"})]})}},5080:function(e,t,a){e.exports=a.p+"static/media/logo.6d23425eb8422d4c60b9.png"}}]);
-//# sourceMappingURL=929.10c4909c.chunk.js.map
